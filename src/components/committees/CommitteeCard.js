80caeaa566142bce6cb28307dbae59a2e7620aae
Baris Mutlu <baris@mutlu.dev>
2024-03-13 18:41:19 +0300

    Make Committee routes dynamic and change a tags to Link

--- src/components/committees/CommitteeCard.js
+++ src/components/committees/CommitteeCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.css";
 
 const CommitteeCard = ({ props }) => {
   return (
     <div className="col col-lg-4">
       <div className="card h-100 committee-card" >
-        <a href={"/" + props.shortname} ><img src={require(`../../img/committees/${props.shortname.toLowerCase()}.png`)} alt={props.shortname} className="committee-img"  /></a>
+        <Link to={props.path} ><img src={require(`../../img/committees/${props.shortname.toLowerCase()}.png`)} alt={props.shortname} className="committee-img"  /></Link>
       </div>
     </div>
   )
